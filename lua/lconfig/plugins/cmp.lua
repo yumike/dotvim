d05602e5aba55485246a819cfec558ee37be153d
Mike Yumatov <mike.yumatov@gmail.com>
2022-06-28 21:05:11 +0300

    Better mappings for cmp

--- lua/lconfig/plugins/cmp.lua
+++ lua/lconfig/plugins/cmp.lua
@@ -12,6 +12,8 @@ M.config = function()
       end,
     },
     mapping = {
+      ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+      ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
       ['<C-d>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
